c88febc15f15820e6c4da1269c61034ae8dac32f
Asp Dot Net <Asp Dot Net@DESKTOP-7SNQED0>
2022-11-15 15:15:11 +0530

    Add project files.

---
+++ PDFUPLOADED/pdf.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="pdf.aspx.cs" Inherits="PDFUPLOADED.pdf" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="//code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"></script>
+
+    <script type="text/javascript">
+        ("#file-uploader").dxFileUploader({ uploadMode: "instantly", multiple: true, invalidFileExtensionMessage: "type not allowed", })
+onUploaded: function (e) {  
+     var files = this.option("value");  
+           for (var i = 0; i < files.length; i++)  
+            {  
+               if (e.file.name == files[i].name)  
+               {  
+                  files.splice(i,1);  
+             }  
+           }  
+ this.option("value", files);  
+}  
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="text-align:center">
+            <asp:Label ID="Label1" runat="server" Text="Uplaod "></asp:Label>
+            <br />
+            <asp:FileUpload ID="FileUpload1" runat="server" ToolTip="Select Only pdf File" AllowMultiple="True" />
+            <br />
+            <asp:Button ID="Button1" runat="server" onchange="onUploaded(this)" OnClick="Button1_Click" Text="Upload" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" Text="View Data File" OnClick="Button2_Click" />
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+
+  </div>
+            <asp:GridView ID="GridView1" runat="server" Caption="Show Data " Width="100%" CaptionAlign="Top" HorizontalAlign="Justify" DataKeyNames="id" onselectedindexchanged="GridView1_SelectedIndexChanged" ToolTip="Excel FIle DownLoad Tool" CellPadding="2" ForeColor="Black" GridLines="None" BackColor="LightGoldenrodYellow" BorderColor="Tan" BorderWidth="1px">  
+            <Columns>  
+                <asp:CommandField ShowSelectButton="True" SelectText="Download" ControlStyle-ForeColor="Blue" > 
+<ControlStyle ForeColor="Blue"></ControlStyle>
+                </asp:CommandField>
+                </Columns>  
+            <FooterStyle BackColor="Tan" />  
+            <PagerStyle BackColor="PaleGoldenrod" ForeColor="DarkSlateBlue" HorizontalAlign="Center" />  
+            <SelectedRowStyle BackColor="DarkSlateBlue" ForeColor="GhostWhite" />  
+            <HeaderStyle BackColor="Tan" Font-Bold="True" />  
+            <AlternatingRowStyle BackColor="PaleGoldenrod" /> 
+                <SortedAscendingCellStyle BackColor="#FAFAE7" />
+                <SortedAscendingHeaderStyle BackColor="#DAC09E" />
+                <SortedDescendingCellStyle BackColor="#E1DB9C" />
+                <SortedDescendingHeaderStyle BackColor="#C2A47B" />
+            </asp:GridView>  
+             
+        
+    </form>
+</body>
+</html>
---
+++ PDFUPLOADED/pdf.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PDFUPLOADED
+{
+    public partial class pdf : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-7SNQED0\SQLEXPRESS;Initial Catalog=suraj;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            Label2.Visible = true;
+            string filePath = FileUpload1.PostedFile.FileName;
+            string filename1 = Path.GetFileName(filePath);
+            FileUpload1.SaveAs(Request.PhysicalApplicationPath + "/file/" + FileUpload1.FileName.ToString());
+            string ext = Path.GetExtension(filename1);
+            string type = String.Empty;
+            if (!FileUpload1.HasFile)
+            {
+                Label2.Text = "Please Select File";
+            }
+            else
+            if (FileUpload1.HasFile)
+            {
+
+                try
+                {
+                    switch (ext)
+                    {
+                        case ".pdf":
+                            type = ".pdf";
+                            break;
+                        case ".doc":
+                            type = ".doc";
+                            break;
+                        case ".docx":
+                            type = ".docx";
+                            break;
+                    }
+                    if (type != String.Empty && ext == ".pdf" || ext == ".docx" || ext == ".doc")
+                    {
+                        con.Open();
+                        SqlCommand cmd = new SqlCommand("insert into uploadpdff values(@name,@type)", con);
+                        cmd.Parameters.Add("@name", SqlDbType.VarChar).Value =filename1;        
+                        cmd.Parameters.Add("@type", SqlDbType.VarChar).Value = type;
+                        cmd.ExecuteNonQuery();
+                        Label2.ForeColor = System.Drawing.Color.Green;
+                        Label2.Text = "File Uploaded Successfully";
+                    }
+                    else
+                    {
+                        Label2.ForeColor = System.Drawing.Color.Red;
+                        Label2.Text = "Select Only PDF Files ";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Label2.Text = "Error: " + ex.Message.ToString();
+                }
+            }
+        }
+
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from uploadpdff", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select name,type from uploadpdff where id=@id", con);
+            cmd.Parameters.AddWithValue("id", GridView1.SelectedRow.Cells[1].Text);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                Response.Clear();
+                Response.Buffer = true;
+                Response.ContentType = dr["type"].ToString();
+                Response.AddHeader("content-disposition", "attachment;filename=" + dr["name"].ToString());
+                Response.Charset = "";
+                Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                Response.End();
+            }
+        }
+
+    }
+}
